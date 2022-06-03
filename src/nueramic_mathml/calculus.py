fff4e517adc208d5458bac898725c3155f7a82ad
victorbarbarich@gmail.com <Vik32768tor>
2022-06-04 01:08:46 +0300

    add dtype checking and converting

--- src/nueramic_mathml/calculus.py
+++ src/nueramic_mathml/calculus.py
@@ -7,7 +7,7 @@ import torch
 from typing import Callable
 
 
-def gradient(function: Callable[[torch.Tensor], float], x0: torch.Tensor, delta_x: float = 1e-8) -> torch.Tensor:
+def gradient(function: Callable[[torch.Tensor], float], x0: torch.Tensor, delta_x: float = 1e-4) -> torch.Tensor:
     """
     Returns the gradient of the function at a specific point x0
     A two-point finite difference formula that approximates the derivative
@@ -25,16 +25,18 @@ def gradient(function: Callable[[torch.Tensor], float], x0: torch.Tensor, delta_
          \\enspace \\dots \\enspace \\frac{\\partial f}{\\partial x_n}\\right]^\\top
 
     :param function: function which depends on n variables from x
-    :param x0: n x 1 - dimensional array :math:`\\in R^{n}`
+    :param x0: n x 1 - dimensional array :math:`\\in R^{n}`. dtype is torch.double (float64)
     :param delta_x: precision of two-point formula above (delta_x = h)
     :return: vector of partial derivatives
     """
-    x0 = x0.flatten()
-    grad = torch.zeros_like(x0)
-    assert not isinstance(x0, torch.Tensor), 'x0 must be torch.Tensor'
+    x0: torch.Tensor = x0.flatten().double()
+
+    delta_x = torch.tensor(delta_x, dtype=x0.dtype)
+    grad = torch.zeros_like(x0, dtype=x0.dtype)
+    assert isinstance(x0, torch.Tensor), 'x0 must be torch.Tensor'
 
     for i in range(len(x0)):
-        delta = torch.zeros_like(x0, dtype=torch.float64)
+        delta = torch.zeros_like(x0, dtype=x0.dtype)
         delta[i] += delta_x
         grad_i = (function(x0 + delta) - function(x0 - delta)) / (2 * delta_x)
         grad[i] = grad_i
