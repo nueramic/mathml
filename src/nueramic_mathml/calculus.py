199df3314bc38b72978599331e73ffbd171f499f
victorbarbarich@gmail.com <Vik32768tor>
2022-06-04 14:59:17 +0300

    calculus from optimization_ml done

--- src/nueramic_mathml/calculus.py
+++ src/nueramic_mathml/calculus.py
@@ -2,9 +2,9 @@
 # Gradient, Hessian, Jacobian
 # Radial basis functions
 # Logistic function
-
+from __future__ import annotations
 import torch
-from typing import Callable
+from typing import Callable, Sequence
 
 
 def gradient(function: Callable[[torch.Tensor], float], x0: torch.Tensor, delta_x: float = 1e-4) -> torch.Tensor:
@@ -24,16 +24,22 @@ def gradient(function: Callable[[torch.Tensor], float], x0: torch.Tensor, delta_
          \\frac{\\partial f}{\\partial x_2}
          \\enspace \\dots \\enspace \\frac{\\partial f}{\\partial x_n}\\right]^\\top
 
+        >>> gradient(lambda x: (x ** 2).sum(), torch.tensor([1., 2.])).round()  # f(x, y)  = x ** 2 + y ** 2
+            tensor([2., 4.], dtype=torch.float64)
+
     :param function: function which depends on n variables from x
     :param x0: n x 1 - dimensional array :math:`\\in R^{n}`. dtype is torch.double (float64)
     :param delta_x: precision of two-point formula above (delta_x = h)
     :return: vector of partial derivatives
-    """
-    x0: torch.Tensor = x0.flatten().double()
+    .. note::
+        If we make delta_x :math:`\\leq` 1e-4 hessian returns matrix with large error rate
 
-    delta_x = torch.tensor(delta_x, dtype=x0.dtype)
-    grad = torch.zeros_like(x0, dtype=x0.dtype)
+    """
     assert isinstance(x0, torch.Tensor), 'x0 must be torch.Tensor'
+
+    x0: torch.Tensor = x0.flatten().double()
+    delta_x = torch.tensor(max(delta_x, 1e-4), dtype=x0.dtype)
+    grad = torch.zeros_like(x0, dtype=x0.dtype)
 
     for i in range(len(x0)):
         delta = torch.zeros_like(x0, dtype=x0.dtype)
@@ -42,3 +48,62 @@ def gradient(function: Callable[[torch.Tensor], float], x0: torch.Tensor, delta_
         grad[i] = grad_i
 
     return grad
+
+
+def jacobian(f_vector: Sequence[Callable[[torch.Tensor], float]],
+             x0: torch.Tensor,
+             delta_x: float = 1e-4) -> torch.Tensor:
+    """
+    Returns the Jacobian matrix of a sequence of m functions from f_vector by n variables from x.
+
+        >>> func_3 = [lambda x: x[0] ** 2 + x[1], lambda x: 2 * x[0] + 5 * x[1], lambda x: x[0] * x[1]]
+        >>> print(jacobian(func_3, torch.tensor([-1, 2])).round())
+        tensor([[-2.,  1.],
+                [ 2.,  5.],
+                [ 2., -1.]], dtype=torch.float64)
+
+    :param f_vector: a flat sequence, list or tuple or other containing m functions
+    :param x0: an n-dimensional array. The specific point at which we will calculate the Jacobian
+    :param delta_x: precision of gradient
+    :return: the Jacobian matrix according to the above formula. Matrix n x m
+    """
+    assert isinstance(f_vector, Sequence), 'f_vector must be sequence'
+    jac = torch.zeros(len(f_vector), x0.flatten().shape[0], dtype=torch.float64)
+    for j in range(len(f_vector)):
+        jac[j, :] = gradient(f_vector[j], x0, delta_x)
+
+    return jac
+
+
+def hessian(function: Callable[[torch.Tensor], float], x0: torch.Tensor, delta_x: float = 1e-4) -> torch.Tensor:
+    """
+    Returns a hessian of function at point x0
+
+        >>> def paraboloid(x): return x[0] ** 2 + 2 * x[1] ** 2
+        >>> print(hessian(paraboloid, [1, 1]).round())
+        [[2. 0.]
+         [0. 4.]]
+
+    :param function: function which depends on n variables from x
+    :param x0: n - dimensional array
+    :param delta_x: precision of two-point formula above (delta_x = h)
+    :return: the hessian of function
+
+    .. note::
+        If we make delta_x :math:`\\leq` 1e-4 hessian returns matrix with large error rate
+
+    """
+    delta_x = max(delta_x, 1e-4)  # Check note
+    x0: torch.Tensor = x0.flatten().double()
+    hes = torch.zeros(x0.shape[0], x0.shape[0], dtype=torch.float64)
+
+    for i in range(len(x0)):
+        delta_i = torch.zeros_like(x0, dtype=torch.float64)
+        delta_i[i] += delta_x
+
+        def partial_i(x: torch.Tensor) -> float:
+            return (function(x + delta_i) - function(x - delta_i)) / (2 * delta_x)
+
+        hes[i, :] = gradient(partial_i, x0, delta_x)
+
+    return hes
--- tests/calculus_test.py
+++ tests/calculus_test.py
@@ -1,17 +1,104 @@
 import pytest
+import torch
 
-from src.nueramic_mathml.calculus import *
+from src.nueramic_mathml.calculus import gradient, jacobian, hessian
 
-test_functions = [
-    (lambda x: (x ** 2).sum(), torch.tensor([1., 2.]), torch.tensor([2., 4.])),
-    (lambda x: 1, torch.tensor([0., 0.]), torch.tensor([0., 0.])),
-    (lambda x: torch.sin(x[0] * x[1]), torch.tensor([torch.pi / 2, 2]), torch.tensor([-2., -torch.pi / 2])),
-    (lambda x: x.sum(), torch.tensor([5] * 1000), torch.tensor([1] * 1000)),
-    (lambda x: (x ** 2).sum(), torch.arange(1, 1000), torch.arange(1, 1000) * 2),
-    (lambda x: x.prod(), torch.arange(1, 10), torch.arange(1, 10).prod() / torch.arange(1, 10))
+# Tests Gradient
+test_functions_grad = [
+    (
+        lambda x: (x ** 2).sum(),
+        torch.tensor([1., 2.]),
+        torch.tensor([2., 4.])
+    ),
+    (
+        lambda x: 1,
+        torch.tensor([0., 0.]),
+        torch.tensor([0., 0.])
+    ),
+    (
+        lambda x: torch.sin(x[0] * x[1]),
+        torch.tensor([torch.pi / 2, 2]),
+        torch.tensor([-2., -torch.pi / 2])
+    ),
+    (
+        lambda x: x.sum(),
+        torch.tensor([5] * 1000),
+        torch.tensor([1] * 1000)
+    ),
+    (
+        lambda x: (x ** 2).sum(),
+        torch.arange(1, 1000),
+        torch.arange(1, 1000) * 2
+    ),
+    (
+        lambda x: x.prod(),
+        torch.arange(1, 10),
+        torch.arange(1, 10).prod() / torch.arange(1, 10)
+    )
 ]
 
 
-@pytest.mark.parametrize('function, x0, expected', test_functions)
+@pytest.mark.parametrize('function, x0, expected', test_functions_grad)
 def test_gradient(function, x0, expected):
     assert torch.allclose(gradient(function, x0.double()), expected.double(), atol=1e-6, rtol=1e-4)
+
+
+# Tests Jacobian
+test_functions_jac = [
+    (  # test from example
+        [
+            lambda x: x[0] ** 2 + x[1],
+            lambda x: 2 * x[0] + 5 * x[1],
+            lambda x: x[0] * x[1]
+        ],
+        torch.tensor([-1, 2]),
+        torch.tensor([[-2., 1.], [2., 5.], [2., -1.]], dtype=torch.float64)
+    ),
+    (
+        [
+            lambda x: 1
+        ] * 10,
+        torch.arange(-10, 10).double(),
+        torch.zeros(10, 20).double()
+    ),
+    (
+        [
+            lambda x: torch.sin(x).sum(),
+            lambda x: torch.sin(x).sum()
+        ],
+        torch.ones(6) * torch.pi,
+        torch.ones(2, 6, dtype=torch.float64) * -1
+    )
+]
+
+
+@pytest.mark.parametrize('functions, x0, expected', test_functions_jac)
+def test_jacobian(functions, x0, expected):
+    jac = jacobian(functions, x0)
+    assert torch.allclose(jac, expected)
+
+
+# Tests Hessian
+test_functions_hes = [
+    (
+        lambda x: (x ** 2).sum(),
+        torch.tensor([1., 2.]),
+        torch.tensor([[2., 0.], [0., 2.]]).double()
+    ),
+    (
+        lambda x: 1,
+        torch.tensor([0., 0.]),
+        torch.tensor([[0., 0.], [0., 0.]]).double()
+    ),
+    (
+        lambda x: torch.sin(x[0] + x[1]),
+        torch.tensor([torch.pi / 2, torch.pi / 2]),
+        torch.tensor([[0, 0], [0, 0]]).double()
+    ),
+]
+
+
+@pytest.mark.parametrize('function, x0, expected', test_functions_hes)
+def test_hessian(function, x0, expected: torch.Tensor):
+    hes = hessian(function, x0)
+    assert torch.allclose(hes, expected, atol=1e-6, rtol=1e-4)
