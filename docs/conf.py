6be10600e9d850b3996275675171089955966a7d
victorbarbarich@gmail.com <Vik32768tor>
2022-06-05 01:07:37 +0300

    fixed copy-button

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,8 @@ extensions = [
     'sphinx.ext.autosectionlabel',
     'sphinx_copybutton'
 ]
-copybutton_prompt_text = ">>> "
+copybutton_prompt_text = r'>>> |\.\.\. |\$ |'
+copybutton_prompt_is_regexp = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
