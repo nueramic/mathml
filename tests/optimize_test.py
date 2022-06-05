3eaa17d956b12115b07de8199430976a32d671d7
victorbarbarich@gmail.com <Vik32768tor>
2022-06-05 04:26:59 +0300

    spi and brent algs tested

--- tests/optimize_test.py
+++ tests/optimize_test.py
@@ -1,5 +1,5 @@
 import pytest
-from src.nueramic_mathml import golden_section_search
+from src.nueramic_mathml import *
 import numpy
 
 # Test golden section search
@@ -8,10 +8,21 @@ test_functions = [  # func, [a, b], true_point
     (lambda x: x ** 2, (-2, 2), 0, 'parabola'),
     (lambda x: - numpy.exp(numpy.cos(x)), (-2, 2), 0, 'e(cos(x))'),
     (lambda x: - numpy.log(x), (1., 4.), 4, 'log(x)'),
-    (lambda x: - numpy.log(x) + numpy.pi * numpy.sin(x) * x, (2, 7), 4.925548, '-ln(x) + pi sin(x) x')
+    (lambda x: - numpy.log(x) + numpy.pi * numpy.sin(x) * x, (2.5, 7), 4.925548, '-ln(x) + pi sin(x) x')
 ]
 
 
 @pytest.mark.parametrize("function, bounds, expected, name", test_functions)
 def test_golden_section_search(function, bounds, expected, name):
     assert golden_section_search(function, bounds)[0] == pytest.approx(expected, abs=1e-4)
+
+
+@pytest.mark.parametrize("function, bounds, expected, name", test_functions)
+def test_successive_parabolic_interpolation(function, bounds, expected, name):
+    if name != '-ln(x) + pi sin(x) x':  # the test is too rough for sensitive spi
+        assert successive_parabolic_interpolation(function, bounds)[0] == pytest.approx(expected, abs=1e-4)
+
+
+@pytest.mark.parametrize("function, bounds, expected, name", test_functions)
+def test_brent(function, bounds, expected, name):
+    assert brent(function, bounds)[0] == pytest.approx(expected, abs=1e-4)
