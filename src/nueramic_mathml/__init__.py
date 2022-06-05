b8cd85e10d56980fc63f0566b4fb5781d3d57957
victorbarbarich@gmail.com <Vik32768tor>
2022-06-05 04:25:36 +0300

    + 2 methods in __all__

--- src/nueramic_mathml/__init__.py
+++ src/nueramic_mathml/__init__.py
@@ -1,10 +1,13 @@
 from .calculus import hessian, jacobian, gradient
+from .one_optimize import golden_section_search, successive_parabolic_interpolation, brent
 from .support import HistoryBFGS, HistorySPI, HistoryGSS, HistoryBrent
-from .optimize import golden_section_search
+# from .multi_optimize import
 
 __all__ = [
     "hessian",
     "jacobian",
     "gradient",
-    "golden_section_search"
+    "golden_section_search",
+    "successive_parabolic_interpolation",
+    "brent"
 ]
