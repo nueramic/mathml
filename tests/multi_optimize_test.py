824b1e51df1d36116493edb3adebc6422741298a
victorbarbarich@gmail.com <Vik32768tor>
2022-06-08 13:53:34 +0300

    rename methods

--- tests/multi_optimize_test.py
+++ tests/multi_optimize_test.py
@@ -1,7 +1,7 @@
 import pytest
 import torch
 
-from src.nueramic_mathml import bfgs, gd_constant_step, gd_frac_step, gd_optimal_step, nonlinear_cgm
+from src.nueramic_mathml import bfgs, gd_constant, gd_frac, gd_optimal, nonlinear_cgm
 
 test_functions = [  # func, [a, b], true_point
     (lambda x: (x ** 2).sum(), torch.tensor([-2]), torch.tensor([0]), 'parabola'),
@@ -16,19 +16,19 @@ def test_bfgs(function, x0, expected, name):
 
 @pytest.mark.parametrize('function, x0, expected, name', test_functions)
 def test_gd_c(function, x0, expected, name):
-    assert gd_constant_step(function, x0)[0] == pytest.approx(expected, abs=1e-5)
+    assert gd_constant(function, x0)[0] == pytest.approx(expected, abs=1e-5)
 
 
 @pytest.mark.parametrize('function, x0, expected, name', test_functions)
 def test_gd_f(function, x0, expected, name):
-    assert gd_frac_step(function, x0)[0] == pytest.approx(expected, abs=1e-5)
+    assert gd_frac(function, x0)[0] == pytest.approx(expected, abs=1e-5)
 
 
 @pytest.mark.parametrize('function, x0, expected, name', test_functions)
 def test_gd_o(function, x0, expected, name):
-    assert gd_optimal_step(function, x0)[0] == pytest.approx(expected, abs=1e-5)
+    assert gd_optimal(function, x0)[0] == pytest.approx(expected, abs=1e-5)
 
 
 @pytest.mark.parametrize('function, x0, expected, name', test_functions)
-def test_gd_o(function, x0, expected, name):
+def test_gd_cgm(function, x0, expected, name):
     assert nonlinear_cgm(function, x0)[0] == pytest.approx(expected, abs=1e-5)
