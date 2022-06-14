1931dca531235edc6e19b3ab6de79c6b433f1c93
victorbarbarich@gmail.com <Vik32768tor>
2022-06-15 00:21:10 +0300

    + sa + metrics

--- src/nueramic_mathml/ml/__init__.py
+++ src/nueramic_mathml/ml/__init__.py
@@ -1,6 +1,17 @@
 from .optimize import *
+from .metrics import *
+
 
 __all__ = [
     "NueSGD",
-    "fit_by_sgd",
+    "SimulatedAnnealing",
+    "recall",
+    "precision",
+    "f_score",
+    "tpr",
+    "fpr",
+    "best_threshold",
+    "roc_curve",
+    "auc_roc",
+    "binary_classification_report"
 ]
