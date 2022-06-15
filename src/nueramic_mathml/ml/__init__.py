ab2342fe89944f32c05c16f5699fedb2421b9cc8
victorbarbarich@gmail.com <Vik32768tor>
2022-06-15 17:29:27 +0300

    refactoring metrics import

--- src/nueramic_mathml/ml/__init__.py
+++ src/nueramic_mathml/ml/__init__.py
@@ -1,17 +1,10 @@
 from .optimize import *
-from .metrics import *
-
+from .classification import LogisticRegressionRBF, LogisticRegression
+from . import metrics
 
 __all__ = [
     "NueSGD",
     "SimulatedAnnealing",
-    "recall",
-    "precision",
-    "f_score",
-    "tpr",
-    "fpr",
-    "best_threshold",
-    "roc_curve",
-    "auc_roc",
-    "binary_classification_report"
+    "LogisticRegression",
+    "LogisticRegressionRBF"
 ]
