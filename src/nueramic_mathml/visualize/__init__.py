d7907c7df67c7bd01b23a0c3848014f432f2f15f
victorbarbarich@gmail.com <Vik32768tor>
2022-06-19 02:09:44 +0300

    + gen animations

--- src/nueramic_mathml/visualize/__init__.py
+++ src/nueramic_mathml/visualize/__init__.py
@@ -1,8 +1,12 @@
 from .one_animation import gen_animation_gss, gen_animation_spi, gen_animation_brent
-
+from .multi_animation import make_surface, make_contour, gen_animated_surface, gen_simple_gradient
 
 __all__ = [
     "gen_animation_gss",
     "gen_animation_spi",
-    "gen_animation_brent"
+    "gen_animation_brent",
+    "make_surface",
+    "make_contour",
+    "gen_animated_surface",
+    "gen_simple_gradient"
 ]
