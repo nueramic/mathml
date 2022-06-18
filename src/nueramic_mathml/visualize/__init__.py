d8c07ddbf94c868e9e126f8c45ad69bdb8c56507
victorbarbarich@gmail.com <Vik32768tor>
2022-06-18 03:43:52 +0300

    + 3 visualizations od 1d algs

---
+++ src/nueramic_mathml/visualize/__init__.py
@@ -0,0 +1,8 @@
+from .one_animation import gen_animation_gss, gen_animation_spi, gen_animation_brent
+
+
+__all__ = [
+    "gen_animation_gss",
+    "gen_animation_spi",
+    "gen_animation_brent"
+]
