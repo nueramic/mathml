568db8c2262a9b95d462f33dd45e1ffd6e8ac87e
victorbarbarich@gmail.com <Vik32768tor>
2022-06-20 00:49:45 +0300

    + class + regr plots

--- src/nueramic_mathml/visualize/__init__.py
+++ src/nueramic_mathml/visualize/__init__.py
@@ -1,5 +1,6 @@
 from .one_animation import gen_animation_gss, gen_animation_spi, gen_animation_brent
 from .multi_animation import make_surface, make_contour, gen_animated_surface, gen_simple_gradient
+from .ml_animation import gen_classification_plot, gen_regression_plot
 
 __all__ = [
     "gen_animation_gss",
@@ -8,5 +9,7 @@ __all__ = [
     "make_surface",
     "make_contour",
     "gen_animated_surface",
-    "gen_simple_gradient"
+    "gen_simple_gradient",
+    "gen_classification_plot",
+    "gen_regression_plot"
 ]
