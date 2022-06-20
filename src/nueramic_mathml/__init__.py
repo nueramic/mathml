08697342fff688d248dd4c80ac1f1220d0f3dfcb
victorbarbarich@gmail.com <Vik32768tor>
2022-06-20 14:51:55 +0300

    + branch bound

--- src/nueramic_mathml/__init__.py
+++ src/nueramic_mathml/__init__.py
@@ -3,7 +3,7 @@ from .ml import metrics
 from .ml.classification import *
 from .ml.optimize import *
 from .multi_optimize import gd_optimal, gd_frac, gd_constant, nonlinear_cgm, bfgs, primal_dual_interior, \
-    log_barrier_solver, constrained_lagrangian_solver
+    log_barrier_solver, constrained_lagrangian_solver, branch_bound
 from .one_optimize import golden_section_search, successive_parabolic_interpolation, brent
 from .support import HiddenPrints
 
@@ -28,7 +28,8 @@ __all__ = [
     "visualize",
     "calculus",
     "multi_optimize",
-    "one_optimize"
+    "one_optimize",
+    "branch_bound"
 ]
 
-__version__ = '0.75 beta'
+__version__ = '0.75.1 beta'
